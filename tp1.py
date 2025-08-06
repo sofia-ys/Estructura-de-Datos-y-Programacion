efece72913b3ebb01908e53dd6529914dba685cd
sofia yanes sanchez <85056219+sofia-ys@users.noreply.github.com>
2025-08-06 20:03:47 -0300

    comenzando el tp1

---
+++ tp1.py
@@ -0,0 +1,79 @@
+'''ejercicio 1'''
+# a)
+a = 2 + 3 + 1 + 2
+print(f"a) {a}")
+
+# b) 
+b = 2 + 3 * 1 + 2 
+print(f"b) {b}")
+
+# c)
+c = (2 + 3) * 1 + 2
+print(f"c) {c}")
+
+# d)
+d = (2 + 3) * (1 + 2)
+print(f"d) {d}")
+
+# e)
+e = +---6
+print(f"e) {e}")
+
+# f)
+f = -+-+6
+print(f"f) {f}")
+
+# g)
+g = -3 / 2 - 1
+print(f"g) {g}")
+
+# h)
+h = -3 // 2 - 1
+print(f"h) {h}")
+
+
+'''ejercicio 2'''
+# a)
+a = 2 + (3 * (6/2))
+print(f"a) {a}")
+
+# b) 
+b = (4 + 6)/(2 + 3)
+print(f"b) {b}")
+
+# c)
+c = (4/2)**5
+print(f"c) {c}")
+
+# d)
+d = (4/2)**(4 + 2**2)
+print(f"d) {d}")
+
+# e)
+e = (-3)**2
+print(f"e) {e}")
+
+# f)
+f = -(3**2)
+print(f"f) {f}")
+
+
+'''ejercicio 3'''
+
+'''ejercicio 4'''
+
+'''ejercicio 5'''
+
+'''ejercicio 6'''
+
+'''ejercicio 7'''
+
+'''ejercicio 8'''
+
+'''ejercicio 9'''
+
+'''ejercicio 10'''
+
+'''ejercicio 11'''
+
+'''ejercicio 12'''
